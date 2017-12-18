cc79de821f388b4c2b01f412f317e7d545be2f9a
Michael Kim <michaelkim@Michaels-iMac.local>
2017-12-18 15:38:04 -0500

    12/18 Changelog : - Firebase implementation. - Feedback page implementation. - Populate newsfeed tableview with sample values.

--- BYG/Feedback.swift
+++ BYG/Feedback.swift
@@ -7,11 +7,34 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
 class Feedback : UIViewController {
     
+    // Variable declarations.
+    var feedbackReference : DatabaseReference!
+    
+    @IBOutlet weak var feedbackField: UITextView!
+    @IBAction func submitFeedbackButton(_ sender: Any) {
+        // When the 'Submit' button is clicked, get the 'Feedback' reference.
+        feedbackReference = Database.database().reference()
+        
+        // If the feedback field is not empty, add what is written into the database.
+        if feedbackField.text != "" {
+            feedbackReference?.child("Feedback").childByAutoId().setValue(feedbackField.text)
+            
+            feedbackField.text = ""
+        }
+    }
+    
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
 }
--- BYG/Newsfeed.swift
+++ BYG/Newsfeed.swift
@@ -7,11 +7,48 @@
 //
 
 import Foundation
+import FirebaseDatabase
 
-class Newsfeed : UIViewController {
+class Newsfeed : UIViewController, UITableViewDelegate, UITableViewDataSource {
+    
+    // Variable declarations.
+    @IBOutlet weak var announcementTable: UITableView!
+    
+    var announcementReference : DatabaseReference!
+    var handle: DatabaseHandle!
+    
+    var announcementList: [String] = []
+    
+    // Set up table view.
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return announcementList.count
+    }
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: .default, reuseIdentifier: "announcement")
+        cell.textLabel?.text = announcementList[indexPath.row]
+        return cell;
+    }
     
     override func viewDidLoad() {
+        super.viewDidLoad()
+        
         self.view.addGestureRecognizer(self.revealViewController().panGestureRecognizer())
+        
+        // Get the announcement reference and retrieve information.
+        announcementReference = Database.database().reference()
+        
+        handle = announcementReference?.child("Feedback").observe(.childAdded, with: { (snapshot) in
+            // If there are items within the Newsfeed reference, grab it as a string and show it in the tableview.
+            if let item = snapshot.value as? String{
+                self.announcementList.append(item)
+                self.announcementTable.reloadData()
+            }
+        })
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
     }
     
 }
